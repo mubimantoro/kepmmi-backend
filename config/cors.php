69b4d2c5e8ba4cc8f32aaf931c28eb393fa3fc95
bimantoro <muhbimantoro08@gmail.com>
2025-04-14 22:42:27 +0800

    add url local

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,7 @@
 
 return [
     'paths' => ['api/*'],
-    'allowed_origins' => ['*'],  // In production, specify your frontend domain
+    'allowed_origins' => ['http://localhost:5173'],  // In production, specify your frontend domain
     'allowed_methods' => ['*'],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
